4e34c3a88e476fe66cf379d9d87053d35e40ccc1
ga54geh <d.poppek@gmx.de>
2017-02-28 19:28:16 +0100

    - RuntimeTypeAdapter for deserilazing polymorph list objects (Devices)

--- app/src/main/java/livesmart/com/restClient/LivesmartWebserviceInterface.java
+++ app/src/main/java/livesmart/com/restClient/LivesmartWebserviceInterface.java
@@ -1,6 +1,15 @@
 package livesmart.com.restClient;
 
-import livesmart.com.dataModel.Device;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+import java.lang.reflect.Type;
+import java.sql.Timestamp;
+
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -20,12 +29,17 @@ public interface LivesmartWebserviceInterface {
 
     static final String SERVER_URL = "http://192.168.1.22:8080/";
     //static final String SERVER_URL = "http://131.159.214.236:8080/";
-    /**
-     *User login
-     */
+
+    /** User login */
     @FormUrlEncoded
     @retrofit2.http.PUT("LiveSmartWebService/rest/livesmart/login")
     Call<LoginResponse> postLogin(@Field("username") String username, @Field("password") String password);
+
+    /** User login */
+    @retrofit2.http.GET("LiveSmartWebService/rest/livesmart/user/{userId}")
+    Call<UserPOJO> getUserbyUserId(@Path("userId") int userId);
+
+
 
     /**
      * Get device
